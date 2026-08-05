740ae1f57b46749f5c8456e3004489c1f1759b60
CarfiCadu <carficadu@gmail.com>
2026-08-05 16:53:06 -0300

    first commit

---
+++ src/test/java/SpringBoot/passaporte/PassaporteApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.passaporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassaporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
